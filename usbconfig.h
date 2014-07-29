dc92740047b01193990585f8bf8468bb8df8304b
Martin Neubauer <martin.neubauer@gmx.de>
2014-07-29 16:55:42 +0200

    Configuration for USB ASP

--- usbconfig.h
+++ usbconfig.h
@@ -23,7 +23,7 @@ the newest features and options.
 
 /* ---------------------------- Hardware Config ---------------------------- */
 
-#define USB_CFG_IOPORTNAME      D
+#define USB_CFG_IOPORTNAME      B
 /* This is the port where the USB bus is connected. When you configure it to
  * "B", the registers PORTB, PINB and DDRB will be used.
  */
@@ -31,7 +31,7 @@ the newest features and options.
 /* This is the bit number in USB_CFG_IOPORT where the USB D- line is connected.
  * This may be any bit in the port.
  */
-#define USB_CFG_DPLUS_BIT       2
+#define USB_CFG_DPLUS_BIT       1 
 /* This is the bit number in USB_CFG_IOPORT where the USB D+ line is connected.
  * This may be any bit in the port. Please note that D+ must also be connected
  * to interrupt pin INT0!
